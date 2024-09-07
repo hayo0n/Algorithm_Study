3377828547d376e746853eef98388a3898b37fac
Lee Hayun / 이하윤 <109056326+hayo0n@users.noreply.github.com>
2024-09-07 14:12:24 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,18 +2,5 @@ def solution(array, commands):
     answer = []
     for com in commands:
         i, j, k = com
-        answer.append(list(sorted(array[i-1:j])[k-1]))
-    return answer
-
-2
-3
-4
-5
-6
-7
-def solution(array, commands):
-    answer = []
-    for command in commands:
-        i,j,k = command
-        answer.append(list(sorted(array[i-1:j]))[k-1])
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
